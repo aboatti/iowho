2fcde3d5236551b061416badf50dc5b332949bd5
aboatti <aboatti@gmail.com>
2013-06-27 19:48:40 -0400

    adding to register

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -1,9 +1,24 @@
 <?php
 class Register extends CI_Controller {
 
-	public function index()
+	function index()
+	{
+		$this->load->helper(array('form', 'url'));
+
+		$this->load->library('form_validation');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('register');
+		}
+		else
+		{
+			$this->load->view('registersuccess');
+		}
+	}
+	/*public function index()
 	{
 		$this->load->view('register');
-	}
+	}*/
 }
 ?>
