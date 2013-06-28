7d1eae124e76c2a9af19dd2de61da3b574b4e2e5
aboatti <aboatti@gmail.com>
2013-06-27 20:41:52 -0400

    more validation

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -7,10 +7,9 @@ class Register extends CI_Controller {
 
 		$this->load->library('form_validation');
 
-		$this->form_validation->set_rules('username', 'Username', 'required');
-		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]');
+		$this->form_validation->set_rules('password', 'Password', 'required|matches[passconf]|min_length[8]|callback_password_check');
 		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
-		$this->form_validation->set_rules('email', 'Email', 'required');
 		
 		if ($this->form_validation->run() == FALSE)
 		{
@@ -19,6 +18,15 @@ class Register extends CI_Controller {
 		else
 		{
 			$this->load->view('registersuccess');
+		}
+	}
+	
+	public function password_check($str){
+		if($str == $password){
+			$this->form_validation->set_message('password_check', 'Your password cannot be "Password"');
+			return false;
+		}else{
+			return true;
 		}
 	}
 	/*public function index()
--- application/views/register.php
+++ application/views/register.php
@@ -3,21 +3,19 @@
 <title>Register for Xpens.es</title>
 </head>
 <body>
-<?php echo validation_errors(); ?>
+<?php echo validation_errors('<div class="error">', '</div>'); ?>
 
 <?php echo form_open('register'); ?>
 
-<h5>Username</h5>
-<input type="text" name="username" value="" size="50" />
+
+<h5>Email Address</h5>
+<input type="text" name="email" value="<?php echo set_value('email'); ?>" size="50" />
 
 <h5>Password</h5>
 <input type="text" name="password" value="" size="50" />
 
 <h5>Password Confirm</h5>
 <input type="text" name="passconf" value="" size="50" />
-
-<h5>Email Address</h5>
-<input type="text" name="email" value="" size="50" />
 
 <div><input type="submit" value="Submit" /></div>
 
