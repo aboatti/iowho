4059cb90cfd24064d5ba585a36e5a02d1ec7c538
Andrew Boatti <aboatti@gmail.com>
2013-06-28 16:14:35 +0000

    adding group controller

---
+++ application/controllers/groups.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Groups extends CI_Controller {
+
+	function index()
+	{
+	
+		$this->load->database();
+		$this->load->helper(array('form', 'url'));
+		
+		$this->load->library('form_validation');
+		
+		$this->load->model('User');
+		
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[8]');
+		
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('login');
+		}
+		else
+		{
+			
+			if($userID = $this->User->validateUser($this->input->post('email'),$this->input->post('password'))){
+				$sessionData = array('user_id' => $userID,"logged_in" => TRUE);
+				$this->load->view('loginsuccess');				
+			}else{
+				$this->form_validation->set_message('index', 'Login Failed');
+				$this->load->view('login');
+			}
+		}
+	}
+
+}
+	
+?>
