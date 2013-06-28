a0df624f60bd70c0ad66fe45b7558f702c9a91a4
aboatti <aboatti@gmail.com>
2013-06-27 20:02:20 -0400

    adding validation

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -7,6 +7,11 @@ class Register extends CI_Controller {
 
 		$this->load->library('form_validation');
 
+		$this->form_validation->set_rules('username', 'Username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		
 		if ($this->form_validation->run() == FALSE)
 		{
 			$this->load->view('register');
