320301c3d150176b05c4397465d2bb4f771512f3
aboatti <aboatti@gmail.com>
2013-06-29 15:50:44 -0400

    adding link

--- application/views/loginsuccess.php
+++ application/views/loginsuccess.php
@@ -7,7 +7,8 @@
 <h3>Welcome, you've successfully logged in!</h3>
 
 Would you like to <a href='/groups/create'>create a group?</a>
-
+<br>
+Or <a href='/groups'> view you're part of?</a>
 <!--<p><?php echo anchor('form', 'Try it again!'); ?></p>-->
 
 </body>
